28312e66654fa0cfd7a5dcf1911e7f95d5f49bbf
Rakesh <chipparakesh01@gmail.com>
2023-10-01 23:30:58 +0530

    task manager

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,17 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Task } from './model/task';
-import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http'
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  headers: HttpHeaders;
   API_HOST: string = 'http://localhost:3000/api';
 
   constructor(private readonly httpClient: HttpClient) {
-    this.headers = new HttpHeaders({ 'content-type': 'application/json' })
   }
   createTask(task: Task): Observable<Task> {
     return this.httpClient.post<Task>(this.API_HOST + '/tasks', task)
